d1089a688dda3553b0d9ad2b94a12ec77cf54860
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-11 15:34:08 +0500

    architecture of personal_library_manager completed

--- personal_library_manager.py
+++ personal_library_manager.py
@@ -8,12 +8,23 @@ def menu() -> None:
     4. Display all books  
     5. Display statistics  
     6. Exit  
-    
+
     Enter your choice: """
     ))
     match choice:
         case "1":
             add_a_book()
+        case "2":
+            remove_a_book()
+        case "3":
+            search_for_a_book()
+        case "4":
+            display_all_books()
+        case "5":
+            display_statics()
+        case "6":
+            print("Library saved to file. Goodbye!")
+            exit()
         case _:
             print("Invalid choice. Please try again.")
             menu()
@@ -26,5 +37,41 @@ def add_a_book() -> None:
     genre: str = str(input("Enter the genre: "))
     read_this_book: str = str(input("Have you read this book? (yes/no): "))
     print(book_title, book_author, publication_year, genre, read_this_book)
+
+def remove_a_book() -> None:
+    title_of_book: str = str(input("Enter the title of the book to remove: "))
+    print(title_of_book)
+
+def search_for_a_book() -> None:
+    search_by: str = str(input("""
+    Search by:  
+    1. Title  
+    2. Author
+                               
+    Enter your choice: """))
+    print(search_by)
+
+    match search_by:
+        case "1":
+            title_of_book: str = str(input("Enter the title of the book: "))
+            search_by_title(title_of_book)
+        case "2":
+            author_of_book: str = str(input("Enter the author of the book: "))
+            search_by_author(author_of_book)
+        case _:
+            print("Invalid choice. Please try again.")
+            search_for_a_book()
+
+def search_by_title(title: str) -> None:
+    print("Search by title", title)
+
+def search_by_author(author: str) -> None:
+    print("Search by author", author)
+
+def display_all_books() -> None:
+    print("Display all books")
+
+def display_statics() -> None:
+    print("Display statics")
 
 menu()
